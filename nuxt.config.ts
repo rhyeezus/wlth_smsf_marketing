0a98d1a7c3002247f97c3be8dc3cc6eeb00ce3eb
rhyeezus <rhysgourley@gmail.com>
2026-04-16 16:50:07 +1000

    migrated layer from npm package to github nature fetch, added github token to runtime

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 export default defineNuxtConfig({
-  extends: ['@wlth/design-system'],
   css: ['~/assets/css/global.css'],
+
   runtimeConfig: {
     public: {
       hubspotPortalId: process.env.NUXT_PUBLIC_HUBSPOT_PORTAL_ID ?? '',
+      githubToken: process.env.GITHUB_TOKEN ?? '',
     },
   },
+
+  extends: [
+    ['github:rhyeezus/wlth-design-system#v1.0.0', {auth: process.env.GITHUB_TOKEN ?? '', install: true}]
+  ],
+
+  modules: ['@nuxt/ui']
 })
